2cc1cf4438baae11da7d1381130389d31a856b6f
arjun <arjun.n.elearning@gmail.com>
2026-07-21 11:41:15 +0530

    feature: email template update

---
+++ src/config/envConstants.ts
@@ -0,0 +1,5 @@
+export const DOMAIN_URL = import.meta.env.VITE_DOMAIN_URL
+export const EMAILJS_TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID
+export const EMAILJS_PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+export const EMAILJS_SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID
+export const EMAIL_URL = import.meta.env.VITE_EMAIL_URL
